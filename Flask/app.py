7f76ee07df349add5cbb9690406c223a7d0bdbf3
Abdullah Arif <arifabdullah764@gmail.com>
2025-07-10 18:42:37 +0500

    variable rules and dynamic urls

--- Flask/app.py
+++ Flask/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-
+# wsgi application
 app = Flask(__name__)
 
 # from markupsafe import escape
@@ -7,9 +7,12 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return f"Hello!"
-@app.route("/new")
-def hello_2():
-    return f"Hello, this is a new route!"
+@app.route("/new/<int:num>")
+def num_check(num):
+    if num % 2 == 0:
+        return f"{num} is even"
+    else:
+        return f"{num} is odd"
 # main function
 if __name__ == "__main__":
     app.run(debug=True)
