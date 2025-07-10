b9dfbe079a19e9245020f8fbd28da082147a58da
Abdullah Arif <arifabdullah764@gmail.com>
2025-07-10 18:29:38 +0500

    two pages/routes tried

--- Flask/app.py
+++ Flask/app.py
@@ -2,9 +2,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# from markupsafe import escape
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def hello():
+    return f"Hello!"
+@app.route("/new")
+def hello_2():
+    return f"Hello, this is a new route!"
 # main function
 if __name__ == "__main__":
     app.run(debug=True)
+    
