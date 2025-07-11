6ed18c8abbd588f56c9daf1bede382c5d98cedbd
Abdullah Arif <arifabdullah764@gmail.com>
2025-07-11 18:45:42 +0500

    Add result page and enhance form submission with data validation

--- Flask/main.py
+++ Flask/main.py
@@ -1,6 +1,7 @@
 # integrating html with flask and jinja 2 techniques
 # http verbs like get and post
 from flask import Flask, redirect, url_for, render_template, request
+from markupsafe import escape
 # wsgi application
 app = Flask(__name__)
 
@@ -18,19 +19,32 @@ def about():
 @app.route('/contact')
 def contact():
     return "This is the contact page."
-@app.route('/<name>')
-def user(name):
-    return f"Hello, {name}!"
+@app.route('/<naam>')
+def user(naam):
+    return f"Hello, {naam}!"
 @app.route('/<int:id>')
 def user_id(id):
     return f"User ID: {id}"
-@app.route('/form', methods=['POST', 'GET'])
-def form():
+@app.route('/submit', methods=['POST', 'GET'])
+def submit():
     if request.method == 'POST':
-        name = request.form['name']
-        return f"Submitted name: {name}"
+        name = escape(request.form['name'])
+        roll = escape(request.form['roll'])
+        tenth = escape(float(request.form['tenth']))
+        twelfth = escape(float(request.form['twelfth']))
+        grad = escape(float(request.form['grad']))  
+        return redirect(url_for('res', name=name, roll=roll, tenth=tenth, twelfth=twelfth, grad=grad))
     return render_template('index.html')
+
+@app.route('/result/<string:name>/<string:roll>/<float:tenth>/<float:twelfth>/<float:grad>')
+def res(name, roll, tenth, twelfth, grad):
+    
+    score = 0.3*tenth + 0.3*twelfth + 0.4*grad
+    if score >= 60:
+        mesg = f"Name: {name}, Roll: {roll}, Your score is {score}. You are eligible for placement."
+    else:
+        mesg = f"Name: {name}, Roll: {roll}, Your score is {score}. You are not eligible for placement."
+    return render_template('result.html', message=mesg)
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
