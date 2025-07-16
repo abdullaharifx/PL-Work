36a12ae9abf51cd05ffb2b427eceb499b7dbd902
Abdullah Arif <arifabdullah764@gmail.com>
2025-07-16 14:08:59 +0500

    Fix import statements formatting for consistency

--- flask-todo-app/app.py
+++ flask-todo-app/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
-from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Flask, render_template, request, redirect, url_for, flash, session # type: ignore
+from flask_sqlalchemy import SQLAlchemy # type: ignore
+from werkzeug.security import generate_password_hash, check_password_hash # type: ignore
 from datetime import datetime
 import os
 from functools import wraps
