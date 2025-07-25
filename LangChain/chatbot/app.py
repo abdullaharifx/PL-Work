e65fdef66519e6a5f7cfe579c22a5e5f5e876cd3
Abdullah Arif <arifabdullah764@gmail.com>
2025-07-26 00:01:48 +0500

    Refactor environment variable setup and enhance chatbot functionality with LangChain tracing

--- LangChain/chatbot/app.py
+++ LangChain/chatbot/app.py
@@ -8,8 +8,12 @@ from dotenv import load_dotenv
 # streamlit
 import streamlit as st
 
-os.environ["LANGCHAIN_API_KEY"]  = os.getenv("LANGCHAIN_API_KEY")
+
 os.environ["LANGCHAIN_PROJECT_ID"] = os.getenv("LANGCHAIN_PROJECT_ID")
+
+# langsmit tracking
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_API_KEY"]  = os.getenv("LANGCHAIN_API_KEY")
 
 
 ## prompt template
@@ -23,6 +27,9 @@ prompt = ChatPromptTemplate.from_messages(
 ## streamlit framework
 st.title("LangChain AI Chatbot")
 input_text = st.text_input("Ask a question:")
+
+
+# llm
 llm = ChatGoogleGenerativeAI(
     model="models/gemini-1.5-flash",
     google_api_key=os.getenv("GOOGLE_API_KEY"),
@@ -30,3 +37,19 @@ llm = ChatGoogleGenerativeAI(
     max_output_tokens=1024,
     top_p=0.8,
 )
+
+# parser
+
+output_parser = StrOutputParser()
+
+
+# chain em all
+
+chain = prompt | llm | output_parser
+
+if input_text:
+    # run the chain
+    response = chain.invoke({"input": input_text})
+    
+    # display the response
+    st.write("Response:", response)
